72198925d54fe9bab94c9f670786bbf2d685d7b7
Nick Blumenthal <nrblum01@gmail.com>
2015-02-03 16:41:00 -0800

    Adds maps and ability to draw a route

--- app/assets/javascripts/views/routes/route_new.js
+++ app/assets/javascripts/views/routes/route_new.js
@@ -1,4 +1,8 @@
 TeamRun.Views.RouteNew = Backbone.CompositeView.extend({
+  initialize: function() {
+    this.markers = [];
+  },
+
   template: JST['routes/new'],
 
   render: function() {
@@ -10,6 +14,45 @@ TeamRun.Views.RouteNew = Backbone.CompositeView.extend({
 
   renderMap: function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoibnJibHVtMDEiLCJhIjoicEZqaW0tWSJ9.H00z3GVyiZ8Y68LDvb8rbw';
-    var map = L.mapbox.map('map', 'nrblum01.l4bfc7g0').setView([40, -74.50], 9);
+    this.map = L.mapbox.map('map', 'nrblum01.l4bfc7g0').setView([37.78, -122.40], 14);
+    this.addMapEvents();
+  },
+
+  addMapEvents: function() {
+    this.map.on('click', this.addPoint.bind(this));
+  },
+
+  addPoint: function(mapEvent) {
+    var latlng = mapEvent.latlng;
+    var marker = L.marker(latlng);
+    this.markers.push(marker);
+    marker.addTo(this.map);
+    this.drawPath();
+  },
+
+  drawPath: function() {
+    var latlngs = [];
+    this.markers.forEach(function(marker) {
+      latlngs.push(marker.getLatLng());
+    });
+    var polyline = L.polyline(latlngs, {color: 'red'}).addTo(this.map);
+    this.showDistance();
+  },
+
+  showDistance: function() {
+    var distance = this.calculateDistance();
+    this.$('#route-distance').html(distance + 'm');
+  },
+
+  calculateDistance: function() {
+    var distance = 0;
+    for(var i = 0; i < this.markers.length - 1; i++) {
+      var ptA = this.markers[i].getLatLng();
+      var ptB = this.markers[i + 1].getLatLng();
+      distance += ptA.distanceTo(ptB);
+    }
+
+    return Math.round(distance*100) / 100;
   }
+
 });
